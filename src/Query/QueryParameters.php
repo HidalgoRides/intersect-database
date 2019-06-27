0b936835e942a9480b43e9fb47772e2b9550f814
Tim Kipp <timkipp.22.developer@gmail.com>
2019-06-26 23:12:01 -0700

    Removed unused whereConditions property in QueryParameters

--- src/Query/QueryParameters.php
+++ src/Query/QueryParameters.php
@@ -17,7 +17,6 @@ class QueryParameters {
     private $columns = [];
     private $limit;
     private $order;
-    private $whereConditions = [];
     private $queryConditions = [];
 
     public function __construct()
@@ -58,11 +57,6 @@ class QueryParameters {
         return $this;
     }
 
-    public function getWhereConditions()
-    {
-        return $this->whereConditions;
-    }
-
     /** @return QueryCondition[] */
     public function getQueryConditions()
     {
@@ -71,35 +65,30 @@ class QueryParameters {
 
     public function between($key, $startValue, $endValue)
     {
-        $this->whereConditions[] = [$key, 'BETWEEN', [$startValue, $endValue]];
         $this->queryConditions[] = new BetweenCondition($key, $startValue, $endValue);
         return $this;
     }
     
     public function betweenDates($key, $startDate, $endDate)
     {
-        $this->whereConditions[] = [$key, 'BETWEEN DATES', [$startDate, $endDate]];
         $this->queryConditions[] = new BetweenDatesCondition($key, $startDate, $endDate);
         return $this;
     }
 
     public function equals($key, $value)
     {
-        $this->whereConditions[] = [$key, '=', $value];
         $this->queryConditions[] = new EqualsCondition($key, $value);
         return $this;
     }
 
     public function notEquals($key, $value)
     {
-        $this->whereConditions[] = [$key, '!=', $value];
         $this->queryConditions[] = new NotEqualsCondition($key, $value);
         return $this;
     }
 
     public function like($key, $value)
     {
-        $this->whereConditions[] = [$key, 'LIKE', $value];
         $this->queryConditions[] = new LikeCondition($key, $value);
         return $this;
     }
@@ -119,21 +108,18 @@ class QueryParameters {
             }
         }
 
-        $this->whereConditions[] = [$key, 'IN', $values];
         $this->queryConditions[] = new InCondition($key, $values);
         return $this;
     }
 
     public function isNull($key)
     {
-        $this->whereConditions[] = [$key, 'IS NULL'];
         $this->queryConditions[] = new NullCondition($key);
         return $this;
     }
 
     public function isNotNull($key)
     {
-        $this->whereConditions[] = [$key, 'IS NOT NULL'];
         $this->queryConditions[] = new NotNullCondition($key);
         return $this;
     }
